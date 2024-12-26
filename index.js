0c102e9c4ab63784022244f048fe1c14485499e7
ebhoom1 <info.ebhoom@gmail.com>
2024-12-26 15:53:59 +0530

    Updating to deployment

--- index.js
+++ index.js
@@ -2,46 +2,51 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const bodyParser=require('body-parser');
+const bodyParser = require("body-parser");
+const path = require("path");
 
-
+// Import Routes
 const userR = require("./router/userR");
 const adminR = require("./router/adminR");
-const authR=require("./router/authR");
-const flowchartR=require("./router/flowchartR");
-const EmissionFactorRoute = require("./router/EmissionFactor")
-const gwpRoutes = require('./router/gwpRoutes');
-const fuelCombustionRoutes = require('./router/fuelCombustionRoutes');
-const CountryemissionFactorRouter = require('./router/countryemissionFactorRouter');
-
+const authR = require("./router/authR");
+const flowchartR = require("./router/flowchartR");
+const EmissionFactorRoute = require("./router/EmissionFactor");
+const gwpRoutes = require("./router/gwpRoutes");
+const fuelCombustionRoutes = require("./router/fuelCombustionRoutes");
+const CountryemissionFactorRouter = require("./router/countryemissionFactorRouter");
 
 dotenv.config();
 
 const app = express();
 
+// Middleware
 app.use(express.json());
-// app.use(bodyParser.json({ limit: '50mb' }));
-// app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
-const corsOptions = {
+app.use(cors({
     origin: ["http://localhost:3000", "http://13.232.145.124"],
     credentials: true,
-};
-app.use(cors(corsOptions));
+}));
 
-
-app.use("/api/user", userR);
-app.use("/api/admin",adminR); 
-app.use("/api/auth",authR);
-app.use('/api/flowchart',flowchartR); 
-app.use('/api',EmissionFactorRoute)
-app.use('/api/gwp',gwpRoutes);
-app.use('/api/fuelCombustion',fuelCombustionRoutes);
 // Routes
-app.use('/api/country-emission-factors', CountryemissionFactorRouter);
+app.use("/api/user", userR);
+app.use("/api/admin", adminR);
+app.use("/api/auth", authR);
+app.use("/api/flowchart", flowchartR);
+app.use("/api", EmissionFactorRoute);
+app.use("/api/gwp", gwpRoutes);
+app.use("/api/fuelCombustion", fuelCombustionRoutes);
+app.use("/api/country-emission-factors", CountryemissionFactorRouter);
 
+// Connect to Database
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+// Serve Static Files
+app.use(express.static(path.join(__dirname, "build")));
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`)); 
+// Catch-All Route
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
