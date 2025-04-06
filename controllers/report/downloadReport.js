2711e05c16501b66e9a60c08ba50ee48264224cd
ebhoom1 <info.ebhoom@gmail.com>
2025-04-07 01:24:40 +0530

    changed the const browser line in the download report

--- controllers/report/downloadReport.js
+++ controllers/report/downloadReport.js
@@ -39,8 +39,10 @@ exports.downloadReport = async (req, res) => {
     // const browser = await puppeteer.launch({ headless: "new" });
     const browser = await puppeteer.launch({
       headless: "new",
+      executablePath: "/usr/bin/chromium-browser",
       args: ["--no-sandbox", "--disable-setuid-sandbox"]
-    });    
+    });
+        
     
     const page = await browser.newPage();
     await page.setContent(htmlContent, { waitUntil: "load" });
